50d06b800fd6b7fb6d25f4f1e285f44ca57d9ba0
YennamAkhileshReddy <akhileshreddy369@gmail.com>
2019-07-23 19:52:04 -0400

    second

--- src/main/java/com/sam/proj/MainController.java
+++ src/main/java/com/sam/proj/MainController.java
@@ -33,6 +33,7 @@ AccountService accountService;
     @RequestMapping(value = "/accou",method = RequestMethod.POST)
 public Account createAccount(@Valid  @RequestBody Account account) {
   Account aco = accountService.createAccount(account);
+        System.out.println("done");
    return aco;
 
 }
